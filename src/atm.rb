22872a7db3222131d05e4dc95a28f2d795b7a061
Fraser <Fraser.Hughes@btinternet.com>
2021-08-31 16:34:23 +0200

    Test and functionality for insufficient funds

--- src/atm.rb
+++ src/atm.rb
@@ -1,27 +1,37 @@
 class Atm
-    attr_accessor :funds
+  attr_accessor :funds
 
-    def initialize
-        @funds = 1000
+  def initialize
+      @funds = 1000
+  end
+
+  def withdraw(amount, account)
+      #We will be using Ruby's "case" - "when" - "when" flow control statement
+      # and check if ther are enough funds in the account
+      case
+      when insufficient_funds_in_account?(amount, account)
+      #we exit the method if the amount we want to withdraw is
+      # bigger than the balance on the account
+          return {status: false, message: "failure", date: Date.today, amount:  amount}
+      else
+          #If it's not, we perform the transaction
+          perform_transaction(amount, account)
+       end
     end
-end
 
-def withdraw(amount, account)
-    #We will be using Ruby's "case" - "when" - "when" flow control statement
-    # and check if ther are enough funds in the account
-    case
-    when amount > account.balance
-    #we exit the method if the amount we want to withdraw is
-    # bigger than the balance on the account
-        return
-    else
-        #If it's not, we perform the transaction
-        #we DEDUCT the amount from the Atm's funds
-        @funds -= amount
-        #We also DEDUCT the amount from the accounts balance
-        account.balance = account.balance - amount
-        #and we return a response for a succesfull withdraw.
-        { status: true, message: "success", date: Date.today, amount: amount}
+    private
+
+    def insufficient_funds_in_account?(amount, account)
+     amount > account.balance
+     end
+
+    def perform_transaction(amount, account)
+      #we DEDUCT the amount from the Atm's funds
+      @funds -= amount
+      #We also DEDUCT the amount from the accounts balance
+      account.balance = account.balance - amount
+      #and we return a response for a succesfull withdraw.
+      {status: true, message: "success", date: Date.today, amount:  amount}
     end
 end
 
