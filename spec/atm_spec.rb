787d918d93325e3f7ce0763f0ca6de45d19e5905
serge_kamya <sergekamya@gmail.com>
2021-08-31 15:45:01 +0200

    updated atm_spec.rb withdraw function

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,35 +1,36 @@
 require "./src/atm.rb"
+require "date"
 describe Atm do
-    it "has 1000$ on initialize" do
-        expect(subject.funds).to eq 1000
-    end
-    it 'funds are reduced at withdraw' do
-        subject.withdraw 50
-        expect(subject.funds).to eq 950
-    end
+  it "has 1000$ on initialize" do
+    expect(subject.funds).to eq 1000
+  end
+  it "funds are reduced at withdraw" do
+    subject.withdraw 50
+    expect(subject.funds).to eq 950
+  end
 
-    let(:account){instance_double('Account')}
+  let(:account) { instance_double("Account") }
 
-    before do
-      #Before each test we need to add an attribute to 'balance'
-      #to the 'account' object and set the value to '100'
-      allow(account).to receive(:balance).and_return(100)
-      #We also need to allow 'account' to receive the new balance
-      #using the setter method 'balance='
-      allow(account).to receive(:balance=)
-    end
+  before do
+    #Before each test we need to add an attribute to 'balance'
+    #to the 'account' object and set the value to '100'
+    allow(account).to receive(:balance).and_return(100)
+    #We also need to allow 'account' to receive the new balance
+    #using the setter method 'balance='
+    allow(account).to receive(:balance=)
+  end
 
-    it 'allow withdraw if the account has enough balance.' do
-        #We need to tell the spec what to look for as a response
-        #and store it in a variable 'expected_outcome'. 
+  it "allow withdraw if the account has enough balance." do
+    #We need to tell the spec what to look for as a response
+    #and store it in a variable 'expected_output'.
 
-        expected_output = {status: true, message: 'success', date: Date.today, amount: 45}
+    expected_output = { status: true, message: "success", date: Date.today, amount: 45 }
 
-        #Need 2 arguments for the withdraw method.
-        #The amount of money to withdraw & the 'account' object.
-        #The reason for the 'account' object is that the ATM needs
-        #the info about the 'accounts' balance to clear the transaction.
+    #Need 2 arguments for the withdraw method.
+    #The amount of money to withdraw & the 'account' object.
+    #The reason for the 'account' object is that the ATM needs
+    #the info about the 'accounts' balance to clear the transaction.
 
-        expect(subject.withdraw(45, account)).to eq expected_outcome
-    end  
+    expect(subject.withdraw(45, account)).to eq expected_output
+  end
 end
--- src/atm.rb
+++ src/atm.rb
@@ -4,9 +4,24 @@ class Atm
     def initialize
         @funds = 1000
     end
+end
 
-    def withdraw(amount)
+def withdraw(amount, account)
+    #We will be using Ruby's "case" - "when" - "when" flow control statement
+    # and check if ther are enough funds in the account
+    case
+    when amount > account.balance
+    #we exit the method if the amount we want to withdraw is
+    # bigger than the balance on the account
+        return
+    else
+        #If it's not, we perform the transaction
+        #we DEDUCT the amount from the Atm's funds
         @funds -= amount
+        #We also DEDUCT the amount from the accounts balance
+        account.balance = account.balance - amount
+        #and we return a response for a succesfull withdraw.
+        { status: true, message: "success", date: Date.today, amount: amount}
     end
 end
 
