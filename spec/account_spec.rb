a88e7b461b02553355d1079cf85b788af3b58182
serge_kamya <sergekamya@gmail.com>
2021-09-01 19:43:12 +0200

    created method for active and not active account owners

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,11 +1,35 @@
 require "./src/account.rb"
 require "date"
 
+
+
 describe Account do
-  
-  it 'is expect to have an expiry date on initalize' do
+
+  let(:person) {instance_double("Person", name: "Thomas")}
+
+  subject { described_class.new({owner: person}) }
+
+  it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+    
+  end
+
+  it "is expect to have an expiry date on initalize" do
     #Setting the validity of the card to 5 years as default
     expect_date = Date.today.next_year(5).strftime("%m/%y")
     expect(subject.set_expire_date).to eq expect_date
+  end
+
+  it "is expected to have :active status on initialize" do
+    expect(subject.account_status).to eq :active
+  end
+  
+  it 'deactivates account using the instance method' do
+    subject.deactivate
+    expect(subject.account_status).to eq :deactivated
+  end
+
+  it 'is expected to raise error if no owner is set' do
+    expect { described_class.new }.to raise_error "An Account owner is required" 
   end
 end
--- src/account.rb
+++ src/account.rb
@@ -1,7 +1,28 @@
 class Account
-STANDARD_VALIDITY_YRS = 5
+  STANDARD_VALIDITY_YRS = 5
+  attr_accessor :account_status
+
+  def initialize(attrs = {})
+    @account_status = :active
+    set_owner(attrs[:owner])
+  end
 
   def set_expire_date
-    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime("%m/%y")
+  end
+
+  #we are using the instance method because we don't need any additional functionality that the class method could give other objects of the class.
+  def deactivate
+    @account_status = :deactivated
+  end
+
+  private
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise "An Account owner is required"
   end
 end
