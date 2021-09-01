eed5a76e9050bc8eb70e5b88cc2ed397efee11a0
serge_kamya <sergekamya@gmail.com>
2021-09-01 10:44:20 +0200

    added method reject card if expired

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -10,7 +10,7 @@ describe Atm do
         expect(subject.funds).to eq 950
     end
 
-    let(:account){instance_double('Account', pin_code: '1234')}
+    let(:account){instance_double('Account', pin_code: 1234, exp_date: "12/22")}
 
     before do
       #Before each test we need to add an attribute to 'balance'
@@ -54,5 +54,11 @@ describe Atm do
     it 'reject withdraw if the pin is wrong' do
         expected_output = {status: false, message: 'wrong pin', date: Date.today}
         expect(subject.withdraw(50, 9999, account)).to eq expected_output
+    end
+
+    it "reject withdraw if the card is expired" do
+        allow(account).to receive(:exp_date).and_return("12/15")
+        expected_output = {status: false, message: "card expired", date: Date.today }
+        expect(subject.withdraw(6, 1234, account)).to eq expected_output
     end
 end
--- src/atm.rb
+++ src/atm.rb
@@ -2,49 +2,55 @@ class Atm
   attr_accessor :funds
 
   def initialize
-      @funds = 1000
+    @funds = 1000
   end
 
-  def withdraw(amount, pin_code, account)
-      #We will be using Ruby's "case" - "when" - "when" flow control statement
-      # and check if ther are enough funds in the account
-      case
-      when insufficient_funds_in_account?(amount, account)
+  def withdraw(amount, pin_code, account) 
+    #We will be using Ruby's "case" - "when" - "when" flow control statement
+    # and check if ther are enough funds in the account
+    case
+    when card_expired?(account.exp_date)
+      { status: false, message: "card expired", date: Date.today }
+    when incorrect_pin?(pin_code, account.pin_code)
+      { status: false, message: "wrong pin", date: Date.today }
+    when insufficient_funds_in_account?(amount, account)
       #we exit the method if the amount we want to withdraw is
       # bigger than the balance on the account
-          return {status: false, message: "failure", date: Date.today, amount:  amount}
-      when insufficient_funds_in_atm?(amount)
-        { status: false, message: "insufficient funds in ATM", date: Date.today }
-      when incorrect_pin?(pin_code,account.pin_code)
-        {status: false, message: 'wrong pin', date: Date.today}  
-      else
-          #If it's not, we perform the transaction
-          perform_transaction(amount, account)
-      end
+      return { status: false, message: "failure", date: Date.today, amount: amount }
+    when insufficient_funds_in_atm?(amount)
+      { status: false, message: "insufficient funds in ATM", date: Date.today }
+    else
+      #If it's not, we perform the transaction
+      perform_transaction(amount, account)
     end
+  end
 
-    private
+  private
 
-    def insufficient_funds_in_account?(amount, account)
-     amount > account.balance
-     end
 
-    def insufficient_funds_in_atm?(amount)
-        amount > @funds
-    end
+  def card_expired?(exp_date)
+    Date.strptime(exp_date, '%m/%y') < Date.today
+  end
 
-    def incorrect_pin?(pin_code, actual_pin)
-      pin_code != actual_pin
-    end
+  def incorrect_pin?(pin_code, actual_pin)
+    pin_code != actual_pin
+  end
 
-    def perform_transaction(amount, account)
-      #we DEDUCT the amount from the Atm's funds
-      @funds -= amount
-      #We also DEDUCT the amount from the accounts balance
-      account.balance = account.balance - amount
-      #and we return a response for a succesfull withdraw.
-      {status: true, message: "success", date: Date.today, amount:  amount}
-    end
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def insufficient_funds_in_atm?(amount)
+    amount > @funds
+  end
+
+
+  def perform_transaction(amount, account)
+    #we DEDUCT the amount from the Atm's funds
+    @funds -= amount
+    #We also DEDUCT the amount from the accounts balance
+    account.balance = account.balance - amount
+    #and we return a response for a succesfull withdraw.
+    { status: true, message: "success", date: Date.today, amount: amount }
+  end
 end
-
-
